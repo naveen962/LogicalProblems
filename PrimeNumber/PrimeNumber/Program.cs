55f4cb4d15e3ee7dd5c3b1a365cf3c12879ee1f1
naveen962 <mrn28897@gmail.com>
2021-09-25 05:28:28 -0700

    prime no program is added

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number");
+            int n = int.Parse(Console.ReadLine());
+            int temp = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    temp=temp+1;
+                }
+            }
+
+            if (temp == 0)
+            {
+                Console.WriteLine(n + "is Prime Number ");
+            }
+            else
+            {
+                Console.WriteLine(n + "is not Prime Number ");
+            }
+        }
+    }
+}
