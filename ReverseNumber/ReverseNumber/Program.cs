23e2f3d9cf4b5c02be8315036a3c0b7849a34798
naveen962 <mrn28897@gmail.com>
2021-09-25 05:31:46 -0700

    Reverse no program is added

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the Number");
+            int n = int.Parse(Console.ReadLine());
+            
+            int rev = 0;
+            while(n!= 0)
+            {
+                int r= n % 10; 
+                n = n / 10;
+                rev = rev * 10 + r;
+            }
+            Console.WriteLine("The Reversed no is : " + rev);
+
+
+        }
+        
+    }
+}
