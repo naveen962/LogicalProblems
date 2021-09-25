806abeb1f9c160d3ba3bca31b2ef34eee31c1d95
naveen962 <mrn28897@gmail.com>
2021-09-25 05:23:02 -0700

    Fibonacci series program is added

---
+++ FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0,b = 1;
+            Console.WriteLine("enter the number");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(a+" "+b);
+            int c;
+            Console.WriteLine("Fibonacci Series are");
+            for(int i=1;i<=n;i++)
+            {
+                c = a + b;
+                Console.WriteLine(" " + c);
+                a = b;
+                b = c;
+
+            }
+        }
+    }
+}
