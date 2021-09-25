de17af9800c50683d8928a5bc412807f605dac7a
naveen962 <mrn28897@gmail.com>
2021-09-25 05:36:12 -0700

    coupon no program is added

---
+++ CoupenNumbers/CoupenNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponNumbers
+{
+    class program
+    {
+        public static void generateCoupon(int n)
+        {
+            Random random = new Random();
+            int num1;
+            int coupon;
+            for (int i = 1; i <= n; i++)
+            {
+                num1 = random.Next(154, 3175);
+                coupon = (1782 * num1) % 1000;
+                Console.WriteLine("coupon {0}-[{1}]", i, coupon);
+            }
+        }
+    
+
+            static void Main(String[] args)
+            {
+
+
+                Console.WriteLine("Enter number to genrate n coupon");
+                int num = int.Parse(Console.ReadLine());
+               
+                program.generateCoupon(num);
+               
+            }
+        }
+    }
+
+
+
+
+
