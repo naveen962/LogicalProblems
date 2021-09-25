c7e1897e24095672b8857bebb3d187a320f04e7e
naveen962 <mrn28897@gmail.com>
2021-09-25 05:38:24 -0700

    stop watch program is added

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        // Create new stopwatch.
+        Stopwatch stopwatch = new Stopwatch();
+
+        // Begin timing.
+        stopwatch.Start();
+
+        // Do something.
+        for (int i = 0; i < 5000; i++)
+        {
+            Thread.Sleep(5);
+            
+        }
+
+        // Stop timing.
+        stopwatch.Stop();
+
+        // Write result.
+        Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+    }
+}
+
