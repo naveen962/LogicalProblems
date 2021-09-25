723b048ba4ec8f59f7fc9be2fe4f3dba32838d9f
naveen962 <mrn28897@gmail.com>
2021-09-25 05:25:54 -0700

    perfect no program is added

---
+++ PerfectNumber/PerfectNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number to check whether Perfectno or Not");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                    sum = sum + i;
+                } 
+            }
+            Console.WriteLine("sum is : " + sum);
+            if (n == sum)
+            {
+                Console.WriteLine(n + "is a PERFECT NUMBER ");
+            }
+
+        }
+    }
+}
